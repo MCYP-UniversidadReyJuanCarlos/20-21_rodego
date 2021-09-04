c84e3ef74be8caea7497601fc52ad4e5b62cb8a4
Roberto Dehesa <robertodehesa@gmail.com>
2021-09-04 11:49:59 +0200

    fix firmware response and rename web

--- firmwalker/firmwalker.py
+++ firmwalker/firmwalker.py
@@ -10,7 +10,7 @@ api = Api(app)
 class Firmwalker(Resource):
     def get(self, path):
         os.system("./firmwalker.sh firmware_data/" + path + " firmware_data/firmwalker_" + path)
-        return jsonify({'message': 'OK'})
+        return jsonify({'message': 'In order to show the results go to: http://localhost:9090/firmware'})
 
 api.add_resource(Firmwalker, '/firmwalker/<string:path>')
 
