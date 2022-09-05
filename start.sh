5545bb70e5bc14b4f6b7f69c696debf617ce3dad
Roberto <r.dehesa.2020@alumnos.urjc.es>
2022-09-05 17:21:48 +0200

    fix script

--- start.sh
+++ start.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 DIRECTORY=data/directories
 if [ ! -d "$DIRECTORY" ]; then
-  echo "$DIRECTORY does not exist 1."
   mkdir -p data/directories
 fi
 
 DIRECTORY_SSL=data/testssl
 if [ ! -d "$DIRECTORY_SSL" ]; then
-  echo "$DIRECTORY_SSL does not exist."
   mkdir -p data/testssl
 fi
 
 DIRECTORY_NMAP=data/nmap
 if [ ! -d "$DIRECTORY_NMAP" ]; then
-  echo "$DIRECTORY_NMAP does not exist."
   mkdir -p data/nmap
 fi
 
